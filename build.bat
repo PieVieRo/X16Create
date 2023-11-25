cff399fe4e2835fb77b0d7556ee5eb876299c832
PieVieRo <tymek24m@gmail.com>
2023-11-25 20:04:56 +0100

    added -Wno-missing-field-initializers
    the compiler throws this warning every time you initialize a color
    struct

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gcc -o X16Create.exe src\main.c -I"C:/Program Files (x86)/raylib/include" -lraylib -lgdi32 -lopengl32 -lwinmm -Wall -Wextra --debug
+gcc -o X16Create.exe src\main.c -I"C:/Program Files (x86)/raylib/include" -lraylib -lgdi32 -lopengl32 -lwinmm -Wall -Wextra -Wno-missing-field-initializers --debug
