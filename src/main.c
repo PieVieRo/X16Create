3c4f146c395fcf6c58a08066b3d435e1517a7ed5
PieVieRo <tymek24m@gmail.com>
2023-11-25 19:15:35 +0100

    changed stuff

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <raylib.h>
+#include <raymath.h>
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
-
 
 typedef struct {
     int pixels;
@@ -14,7 +14,7 @@ int main() {
     InitWindow(screen_width, screen_height, "X16Create");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
-    Sprite sprite = { 8, 25 };
+    Sprite sprite = { 8, 50 };
     while(!WindowShouldClose()) {
         if(IsWindowResized()) {
             screen_width = GetScreenWidth();
@@ -23,11 +23,13 @@ int main() {
         BeginDrawing();
         ClearBackground((Color) {0x18, 0x18, 0x18});
 
-        Vector2 mouse_cell = { 0 };
         int two_thirds_width = screen_width - screen_width/3;
         int grid_size = sprite.pixels * sprite.pixels_size;
-        GuiGrid((Rectangle) { two_thirds_width/2 - grid_size/2 , screen_height / 2 - 100,  grid_size, grid_size }, "", sprite.pixels_size, 1, &mouse_cell);
         GuiGroupBox((Rectangle) { two_thirds_width, screen_height * 0.01, 16*4 + 18*2 + 10, 16*4 + 18*2 + 10 }, "Palette");
+
+        Vector2 mouse_cell = { 0 };
+        GuiGrid((Rectangle) { two_thirds_width/2 - grid_size/2 , screen_height/2 - grid_size/2,  grid_size, grid_size }, "", sprite.pixels_size, 1, &mouse_cell);
+
         EndDrawing();
     }
     CloseWindow();
