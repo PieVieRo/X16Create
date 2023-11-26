9c345e83a2167c3487a76b3891d23d457db253e0
PieVieRo <tymek24m@gmail.com>
2023-11-26 14:46:06 +0100

    moved drawing the canvas to separate function, the canvas now draws the tile

--- src/functions.h
+++ src/functions.h
@@ -6,15 +6,17 @@
 #include "structs.h"
 
 void drawPalette(Palette palette, Vector2* palette_cell) {
-    const int two_thirds_width = GetScreenWidth() - (GetScreenWidth() / 3);
-    const int pixel_size = 15;
+    int screen_width = GetScreenWidth();
+    int screen_height = GetScreenHeight();
+    int two_thirds_width = screen_width - (screen_width / 3);
+    int pixel_size = 15;
     Rectangle grid_rectangle = {
-        two_thirds_width + (GetScreenWidth() - two_thirds_width)/2 - ((pixel_size * 16)/2),
-        GetScreenHeight() * 0.01 + 10,
+        two_thirds_width + (screen_width - two_thirds_width)/2 - ((pixel_size * 16)/2),
+        screen_height * 0.01 + 10,
         pixel_size * 16,
         pixel_size * 16,
     };
-    const int padding = 10;
+    int padding = 10;
     Rectangle group_box_rectangle = {
         grid_rectangle.x - padding,
         grid_rectangle.y - padding,
@@ -29,4 +31,25 @@ void drawPalette(Palette palette, Vector2* palette_cell) {
     GuiGrid(grid_rectangle, NULL, pixel_size, 1, palette_cell);
 }
 
+void drawCanvas(Tile* tile, int canvas_size, Vector2* mouse_cell) {
+    int screen_width = GetScreenWidth();
+    int screen_height = GetScreenHeight();
+    int two_thirds_width = screen_width - (screen_width / 3);
+    int grid_width = tile->width * canvas_size;
+    int grid_height = tile->height * canvas_size;
+    int grid_x = two_thirds_width/2 - grid_width/2;
+    int grid_y = screen_height/2 - grid_height/2;
+    for(int i = 0; i < (tile->width * tile->height); i++) {
+        DrawRectangle(
+                grid_x + ((i % tile->width) * canvas_size),
+                grid_y + ((i / tile->width) * canvas_size),
+                canvas_size,
+                canvas_size,
+                tile->color_data[i]
+                );
+    }
+    GuiGrid((Rectangle) { grid_x, grid_y,  grid_width, grid_height }, "", canvas_size, 1, mouse_cell);
+}
+
 #endif
+
--- src/main.c
+++ src/main.c
@@ -17,21 +17,14 @@ int main() {
     Palette current_palette = { 0 };
     memcpy(current_palette, default_palette, sizeof(Palette));
     while(!WindowShouldClose()) {
-        if(IsWindowResized()) {
-            screen_width = GetScreenWidth();
-            screen_height = GetScreenHeight();
-        }
         BeginDrawing();
         ClearBackground((Color) {0x18, 0x18, 0x18});
 
         Vector2 palette_cell = { 0 };
         drawPalette(current_palette, &palette_cell);
 
-        int two_thirds_width = screen_width - screen_width/3;
-        int grid_width = tile.width * canvas_size;
-        int grid_height = tile.height * canvas_size;
-        Vector2 mouse_cell = { 0 };
-        GuiGrid((Rectangle) { two_thirds_width/2 - grid_width/2 , screen_height/2 - grid_height/2,  grid_width, grid_height }, "", canvas_size, 1, &mouse_cell);
+        Vector2 canvas_cell = { 0 };
+        drawCanvas(&tile, canvas_size, &canvas_cell);
 
         EndDrawing();
     }
